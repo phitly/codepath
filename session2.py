04bc71c28a3b3dbe803c457e64cd783c704a13cd
phitly <phitly@outlook.com>
2025-09-27 13:14:30 -0400

    initial

---
+++ session2.py
@@ -0,0 +1,34 @@
+#Problem 2: Print Doubled List Items
+
+#Write a function doubled() that takes in a list of integers lst as a parameter and prints each item in the list multiplied by two.
+
+#def doubled(lst):
+#    pass
+
+#Example Input: lst = [1,2,3]
+#Example Output:
+
+#2
+#4
+#6
+
+# understanding 
+# can it be both pos and neg ? 
+# can it be a float num ? NO 
+
+
+#plan 
+# 1 define the list out side the func 
+# 2 define the function that takes in the param we created
+# 3 use for loop to iterates thru the list
+# 4 mutilply each iterated item by 2 
+# 5 print our result 
+# 6 call function
+#lst = [1,2,3]
+#def doubled(lst):
+ #   for item in lst:
+     #   item * 2
+  #      print(item)
+#doubled(lst)
+
+
